c1ec200e091bc2aa68347ee320bd7c28297f7574
felipejhordan <felipejordan.alves@gmail.com>
2022-05-04 16:24:34 -0300

    feat(backend): use cors, express.json and create routes in main file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,15 @@
-import express from 'express'
+import cors from 'cors';
+import express from 'express';
+import { routes } from './routes';
 
 const app = express()
 
-app.get("/user", (req, res) => {
-    res.json({
-        api: "OK"
-    })
-})
+app.use(cors())
+
+app.use(express.json())
+
+
+app.use(routes)
 
 app.listen(3333, () => {
     console.log("HTTP server running!")
