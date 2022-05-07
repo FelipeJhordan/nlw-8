dff587bb2d94523937fa088190018dcbc924f147
felipejhordan <felipejordan.alves@gmail.com>
2022-05-07 19:24:54 -0300

    feat(web): update feedback endpoint

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,7 +7,7 @@ import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 export const routes = express.Router()
 
 
-routes.post("/feedbacks", async (req, res) => {
+routes.post("/feedback", async (req, res) => {
     const {type, comment, screenshot} = req.body
 
     const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
